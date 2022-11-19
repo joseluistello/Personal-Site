b8c7461732916ad7d04c07d31e03bf5f07f45212
José Luis Tello <jltello96@gmail.com>
2022-11-19 15:50:31 -0600

    Updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,13 +10,13 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-minimal-blog/gatsby-config.js
-    siteTitle: `Minimal Blog`,
-    siteTitleAlt: `Minimal Blog - Gatsby Theme`,
-    siteHeadline: `Minimal Blog - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://minimal-blog.lekoarts.de`,
-    siteDescription: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and line highlighting.`,
+    siteTitle: `Home`,
+    siteTitleAlt: `Jose Luis Tello`,
+    siteHeadline: `Jose Luis Tello`,
+    siteUrl: `https://joseluistello.gatsby.io`,
+    siteDescription: `Curious about businesses, product, and software.`,
     siteImage: `/banner.jpg`,
-    author: `@lekoarts_de`,
+    author: `@joseluistello`,
   },
   trailingSlash: `never`,
   plugins: [
@@ -26,22 +26,22 @@ module.exports = {
       options: {
         navigation: [
           {
-            title: `Blog`,
+            title: `Library`,
             slug: `/blog`,
           },
           {
-            title: `About`,
+            title: `About me`,
             slug: `/about`,
           },
         ],
         externalLinks: [
           {
-            name: `Twitter`,
-            url: `https://twitter.com/lekoarts_de`,
+            name: `LinkedIn`,
+            url: `https://linkedin.com/in/joseluistello/`,
           },
           {
-            name: `Homepage`,
-            url: `https://www.lekoarts.de?utm_source=minimal-blog&utm_medium=Starter`,
+            name: `Linktree`,
+            url: `https://linktr.ee/joseluistello`,
           },
         ],
       },
@@ -120,7 +120,7 @@ module.exports = {
   }
 }`,
             output: `rss.xml`,
-            title: `Minimal Blog - @lekoarts/gatsby-theme-minimal-blog`,
+            title: `Jose Luis Tello`,
           },
         ],
       },
